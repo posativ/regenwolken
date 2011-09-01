b5774c84448cb0d7fad459246883c9abe3c542fc
posativ <info@posativ.org>
2011-09-01 22:50:24 +0200

    removing optparse
    I am currently experimenting with cross-module global variables, when
    programming more functional. It is pain in the ass with python. Maybe we
    should use werkzeug's environ object, passed through every request
    instead of global importing via `import wolken`...

--- regenwolken.py
+++ regenwolken.py
@@ -19,8 +19,15 @@ from werkzeug.routing import Map, Rule, Submount
 from werkzeug.wsgi import responder
 from werkzeug.datastructures import WWWAuthenticate
 
+# may changed via conf.yaml
+HOSTNAME = "localhost"
+BIND_ADDRESS = "0.0.0.0"
+PORT = 80
+MONGODB_HOST = "127.0.0.1"
+MONGODB_PORT = 27017
+
+# inits global variables HOSTNAME, PORT and so on #FIXME!!
 from wolken import REST, web
-from wolken import HOSTNAME, BIND_ADDRESS, PORT, MONGODB_HOST, MONGODB_PORT
 
 class LimitedRequest(Request):
     # FIXME: Cloud.app can not handle 413 Request Entity Too Large
@@ -35,7 +42,6 @@ HTML_map = Map([
 ])
 
 REST_map = Map([
-    Rule('/', endpoint=REST.index, methods=['GET', 'HEAD']),
     Rule('/', endpoint=REST.upload_file, methods=['POST', ]),
     Rule('/account', endpoint=REST.account),
     Rule('/items', endpoint=REST.items),
@@ -45,45 +51,21 @@ REST_map = Map([
     Rule('/register', endpoint=REST.register, methods=['POST', ]),
 ])
 
+
 @responder
 def application(environ, start_response):
     
-    environ['host'] = 'localhost'
     request = LimitedRequest(environ)
     
     if request.headers.get('Accept', 'application/json') == 'application/json':
         urls = REST_map.bind_to_environ(environ)
     else:
-        #urls = REST_map.bind_to_environ(environ)
-        urls = HTML_map.bind_to_environ(environ)
+        urls = REST_map.bind_to_environ(environ)
+        # urls = HTML_map.bind_to_environ(environ)
 
     return urls.dispatch(lambda f, v: f(environ, request, **v),
                          catch_http_exceptions=True)
 
 if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-    from optparse import OptionParser, make_option
-    
-    options = [
-        make_option('--bind', dest='bind', default='0.0.0.0', type=str, metavar='IP',
-                     help="binding address, e.g. localhost [default: %default]"),
-        make_option('--port', dest='port', default=9000, type=int, metavar='PORT',
-                     help="port, e.g. 80 [default: %default]"),
-        make_option('--mdb-host', dest='mongodb_host', default='localhost',
-                    type=str, metavar='HOST', help="mongoDB host [default: %default]"),
-        make_option('--mdb-port', dest='mongodb_port', default=27017,
-                    type=int, metavar='PORT', help="mongoDB port [default: %default]"),
-        ]
-    
-    parser = OptionParser(option_list=options, usage="usage: %prog [options] [Hostname]")
-    options, args = parser.parse_args()
-    
-    HOSTNAME = args[0] if len(args) == 1 else 'localhost'
-    BIND_ADDRESS = options.bind
-    PORT = options.port
-    
-    MONGODB_HOST = options.mongodb_host
-    MONGODB_PORT = options.mongodb_port
-    
+    from werkzeug.serving import run_simple    
     run_simple(BIND_ADDRESS, PORT, application, use_reloader=True)
-    
