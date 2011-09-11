024ab6885a5b1e5c3bd0538dda594492d765857b
posativ <info@posativ.org>
2011-09-11 23:31:18 +0200

    add manage.py - administrate Regenwolken

---
+++ bin/manage.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2011 posativ <info@posativ.org>. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# The views and conclusions contained in the software and documentation are
+# those of the authors and should not be interpreted as representing official
+# policies, either expressed or implied, of posativ <info@posativ.org>.
+#
+# Regenwolken is a CloudApp clone, with leave the cloud in mind.
+
+__version__ = "0.2"
+
+import sys; reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import logging
+import string
+import re
+
+from datetime import timedelta, datetime
+
+from pymongo import Connection
+from gridfs import GridFS
+from gridfs.errors import NoFile
+
+
+def ppsize(num):
+    '''pretty-print filesize.
+    http://blogmag.net/blog/read/38/Print_human_readable_file_size'''
+    for x in ['bytes','KiB','MiB','GiB','TB']:
+        if num < 1024.0:
+            return "%3.2f %s" % (num, x)
+        num /= 1024.0
+
+    
+def tdelta(input):
+    """converts human-readable time deltas to datetime.timedelta.
+    >>> tdelta(3w 12m) == datetime.timedelta(weeks=3, minutes=12)"""
+    
+    keys = ['weeks', 'days', 'hours', 'minutes']
+    regex = ''.join(['((?P<%s>\d+)%s ?)?' % (k, k[0]) for k in keys])
+    kwargs = {}
+    for k,v in re.match(regex, input).groupdict(default='0').items():
+        kwargs[k] = int(v)
+    return timedelta(**kwargs)
+    
+
+def parse_conf(fp):
+
+    __dict__ = {
+        'HOSTNAME': "localhost",
+        'BIND_ADDRESS': "0.0.0.0",
+        'PORT': 80,
+        'MONGODB_HOST': "127.0.0.1",
+        'MONGODB_PORT': 27017,
+        'ALLOWED_CHARS': string.digits + string.ascii_letters + '.- @',
+        'MAX_CONTENT_LENGTH': 1024*1024*64,
+        'ALLOW_PRIVATE_BOOKMARKS': False,
+    }
+    for line in fp:
+        line = line.strip()
+        if line and not line.startswith('#'):
+            try:
+                key, value = line.split(':', 1)
+                key, value = key.strip(), value.strip()
+            except ValueError:
+                print >> sys.stderr, 'line is wrong `%s`' % line
+                sys.exit(1)
+
+            if value.isdigit():
+                value = int(value)
+            elif value.lower() in ['true', 'false']:
+                value = bool(value.capitalize())
+            __dict__[key.upper()] = value
+            
+    return __dict__
+
+
+def account(conf, options, args):
+    
+    con = Connection(conf['MONGODB_HOST'], conf['MONGODB_PORT'])
+    db = con['cloudapp']
+    fs = GridFS(db)
+    
+    if options.all:
+        query = None
+    elif len(args) == 2:
+        query = {'_id': args[1]} if args[1].isdigit() else {'email': args[1]}
+    else:
+        log.error('accounts <email or _id> requires a valid email or _id')
+        sys.exit(1)
+
+    for acc in db.accounts.find(query):
+        print '%s [id:%s]' % (acc['email'], acc['id'])
+        for key in acc:
+            if key in ['email', '_id', 'id']:
+                continue
+            if key == 'items':
+                try:
+                    size = sum([fs.get(_id).length for _id in acc['items']])
+                except NoFile:
+                    log.warn('Account `%s` has some files missing:', _id)
+                    # fail safe counting
+                    size = 0
+                    missing = []
+                    for i in acc['item']:
+                        if not fs.exists(i):
+                            missing.append(i)
+                        else:
+                            size += fs.get(i).length
+                print'    size: %s' % ppsize(size)
+            print '    %s: %s' % (key, acc[key])
+    if options.all:  print db.accounts.count(), 'accounts total'
+    con.disconnect()
+
+
+def info(conf):
+    '''A basic, incomplete short info.  Displays overall file size and
+    account counts.'''
+    
+    con = Connection(conf['MONGODB_HOST'], conf['MONGODB_PORT'])
+    db = con['cloudapp']
+    fs = GridFS(db)
+    
+    overall_file_size = sum([f['length'] for f in fs._GridFS__files.find()])
+    print fs._GridFS__files.count(), 'files [%s]' % ppsize(overall_file_size)
+    print db.accounts.count(), 'accounts total'
+    
+    con.disconnect()
+    
+def purge(conf, options, args):
+    '''Purges files or accounts.
+    
+    With GNU Opts -a and/or --all a given account/all accounts are removed
+    including metadata and files.
+    
+    Given a crontab-like timedelta e.g. `3d` will remove every file older
+    than 3 days including its metadata. To delete all files write `0d` or --all.'''
+
+    con = Connection(conf['MONGODB_HOST'], conf['MONGODB_PORT'])
+    db = con['cloudapp']
+    fs = GridFS(db)
+    
+    def delete_account(_id):
+        """deletes account with _id and all his files"""
+        
+        items = db.accounts.find_one(_id)['items']
+        db.accounts.remove(_id)
+        
+        for item in items:
+            fs.delete(item)
+            db.items.remove(item)
+        return True
+        
+    if options.account and not options.all and len(args) < 2:
+        log.error('purge -a <_id> requires an account _id')
+        sys.exit(1)
+    elif not options.account and not options.all:
+        log.error('purge <timedelta> requires a time delta')
+        sys.exit(1)
+    
+    if options.account:
+        if options.all:
+            yn = raw_input('delete all accounts? [y/n] ')
+            if yn != 'y':
+                sys.exit(0)
+            print 'deleting %s accounts' % db.accounts.count()
+            for acc in db.accounts.find():
+                delete_account(acc['_id'])
+        else:
+            if not db.accounts.find_one(args[1]):
+                log.error('no such _id `%s`', args[1])
+                sys.exit(1)
+            delete_account(args[1])
+    else:
+        delta = timedelta(0) if options.all else tdelta(args[1])
+        if delta == timedelta(0):
+            yn = raw_input('delete all files? [y/n] ')
+            if yn != 'y':
+                sys.exit(0)
+        else:
+            print 'purging files older than %s' % str(delta)[:-3]
+    
+        now = datetime.utcnow()
+        delete = []
+        for obj in fs._GridFS__files.find():
+            if now - delta  > obj['uploadDate']:
+                delete.append(obj)
+        
+        for cur in db.accounts.find():
+            _id, items = cur['_id'], cur['items']
+            for obj in delete:
+                try:
+                    items.remove(obj['_id'])
+                except ValueError:
+                    pass
+            db.accounts.update({'_id': _id}, {'$set': {'items': items}})
+            
+        for obj in delete:
+            fs.delete(obj['_id'])
+            db.items.remove(obj['_id'])
+
+    con.disconnect()
+    
+
+def repair(conf, options):
+    '''fixes issues created by myself.  Currently, only orphan files and
+    item links are detected and automatically removed.'''
+
+    con = Connection(conf['MONGODB_HOST'], conf['MONGODB_PORT'])
+    db = con['cloudapp']
+    fs = GridFS(db)
+    
+    objs = [obj['_id'] for obj in fs._GridFS__files.find()]
+    meta = [cur['_id'] for cur in db.items.find()]
+    
+    if objs != meta:
+        # 1. metadata has some files missing, no repair possible
+        diff1 = filter(lambda i: not i in objs, meta)
+        diff2 = filter(lambda i: not i in meta, objs)
+        for item in diff1:
+            print 'removing metadata for `%s`' % item
+            db.items.remove(item)
+        
+        # 2. metadata is missing, but file is there. Recover possible, but not implemented #win
+        for item in diff2:
+            objs.remove(item)
+    
+    # rebuild accounts items, when something changed
+    for cur in db.accounts.find():
+        _id, items = cur['_id'], cur['items']
+        items = filter(lambda i: i in objs, items)
+        db.accounts.update({'_id': _id}, {'$set': {'items': items}})
+
+    con.disconnect()
+
+
+if __name__ == '__main__':
+    
+    from optparse import OptionParser, make_option
+    
+    usage = "usage: %prog [options] info|account|purge|repair\n" + '\n' \
+            + "  info    – provides basic information of Regenwolken's MongoDB\n" \
+            + "  account – details of given (email or _id) or --all accounts.\n" \
+            + "  purge   – purge -a account or files. --all works, too.\n" \
+            + "  repair  – repair broken account-file relations in MongoDB"
+
+    options = [
+        make_option('-a', '--account', dest='account', action='store_true',
+                    default=False, help='purge account and its files'),
+        make_option('--all', dest='all', action='store_true',
+                    default=False, help='select ALL'),
+    ]
+    
+    parser = OptionParser(option_list=options, usage=usage)
+    (options, args) = parser.parse_args()
+    
+    log = logging.getLogger('regenwolken')
+    log.addHandler(logging.StreamHandler())
+    log.setLevel(logging.INFO)
+    
+    try:
+        f = open('../conf.yaml', 'r')
+    except IOError:
+        try:
+            f = open('conf.yaml', 'r')
+        except IOError:
+            print >> sys.stderr, 'could not find conf.yaml'
+            sys.exit(1)
+    
+    conf = parse_conf(f)
+    
+    if 'account' in args:
+        account(conf, options, args)
+    elif 'info' in args:
+        info(conf)
+    elif 'purge' in args:
+        purge(conf, options, args)
+    elif 'repair' in args:
+        repair(conf, options)
